cf8f27fdf8f00f4393602d1990ebb9a7611b345c
My Nguyen <my@vnguyen.net>
2018-06-10 19:15:12 -0500

    Add password field for users

---
+++ db/migrate/20180610234735_add_password_field_to_user.rb
@@ -0,0 +1,5 @@
+class AddPasswordFieldToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :password_digest, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_10_224052) do
+ActiveRecord::Schema.define(version: 2018_06_10_234735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2018_06_10_224052) do
     t.string "avatar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "enrollments", "teams"
