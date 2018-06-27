3da6fada1d240718e1de59faa69c10bba5589fb4
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2018-06-27 11:24:43 -0500

    Add tournament staff request specs

---
+++ spec/factories/tournament_staff_factory.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :tournament_staff do
+    role Faker::Number.between(1, 3)
+    user
+    tournament
+  end
+end
--- spec/requests/tournament_staffs_spec.rb
+++ spec/requests/tournament_staffs_spec.rb
@@ -3,10 +3,120 @@
 require "rails_helper"
 
 RSpec.describe "TournamentStaffs", type: :request do
-  describe "GET /tournament_staffs" do
-    it "works! (now write some real specs)" do
-      get tournament_staffs_path
+  describe "GET /tournament_staffs/:id" do
+    it "returns a successful 200 response" do
+      staff = build_stubbed :tournament_staff
+      allow(TournamentStaff).to receive(:find).and_return(staff)
+
+      get tournament_staff_path(staff)
+
       expect(response).to have_http_status(200)
+    end
+
+    it "returns a 404 response" do
+      staff = build_stubbed :tournament_staff
+
+      get tournament_staff_path(staff)
+
+      expect(response).to have_http_status(404)
+    end
+
+    it "returns the user" do
+      staff = build_stubbed :tournament_staff
+      allow(TournamentStaff).to receive(:find).and_return(staff)
+
+      get tournament_staff_path(staff)
+
+      expect(json_response_struct.id).to eq(staff.id)
+    end
+  end
+
+  describe "POST /tournament_staffs" do
+    it "returns a successful 201 created response" do
+      user = create(:user)
+      tournament = create(:tournament)
+      successful_params =
+        { user_id: user.id, tournament_id: tournament.id, role: 1 }
+
+      post tournament_staffs_path,
+        headers: authenticated_header,
+        params: { tournament_staff: successful_params }
+
+      expect(response).to have_http_status(201)
+    end
+
+    it "successfully creates a tournament_staff with the params sent" do
+      user = create(:user)
+      tournament = create(:tournament)
+      successful_params =
+        { user_id: user.id, tournament_id: tournament.id, role: 1 }
+
+      post tournament_staffs_path,
+        headers: authenticated_header,
+        params: { tournament_staff: successful_params }
+
+      expect(json_response_struct.user_id).to eq(user.id)
+      expect(json_response_struct.tournament_id).to eq(tournament.id)
+      expect(json_response_struct.role).to eq(1)
+    end
+  end
+
+  describe "PATCH /tournament_staffs/:id" do
+    it "returns a successful 200 response" do
+      staff = create :tournament_staff, role: 1
+      successful_params = { role: 2 }
+
+      patch tournament_staff_path(staff),
+        headers: authenticated_header,
+        params: { tournament_staff: successful_params }
+
+      expect(response).to have_http_status(200)
+    end
+
+    it "successfully updates the tournament_staff with the params sent" do
+      staff = create :tournament_staff, role: 1
+      successful_params = { role: 2 }
+
+      expect {
+        patch tournament_staff_path(staff), headers: authenticated_header, params: { tournament_staff: successful_params }
+      }.to change { staff.reload.role }.from(1).to(2)
+    end
+
+    it "returns a not found 404 response" do
+      get tournament_staff_path(id: 1)
+
+      expect(response).to have_http_status(404)
+    end
+
+    it "returns an unauthorized 401 response" do
+      patch tournament_staff_path(id: 1)
+
+      expect(response).to have_http_status(401)
+    end
+  end
+
+  describe "DELETE /tournament_staff/:id" do
+    it "returns a successful 204 response" do
+      staff = create(:tournament_staff)
+      delete tournament_staff_path(staff), headers: authenticated_header
+
+      expect(response).to have_http_status(204)
+    end
+
+    it "successfully creates a tournament_staff with the params sent" do
+      staff = create(:tournament_staff)
+
+      expect {
+        delete tournament_staff_path(staff), headers: authenticated_header
+      }.to change { TournamentStaff.count }.from(1).to(0)
+    end
+
+    it "returns a 404 response" do
+      staff = build_stubbed :tournament_staff
+
+      patch tournament_staff_path(staff), headers: authenticated_header
+
+      expect(response).to have_http_status(404)
     end
   end
 end
