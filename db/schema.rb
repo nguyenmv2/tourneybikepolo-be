b7efcabd2c616141edd552ed4cfb7a4e23d4533d
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2018-06-27 22:28:38 -0500

    Set default player_count value to zero

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_26_013909) do
+ActiveRecord::Schema.define(version: 2018_06_28_032752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2018_06_26_013909) do
     t.string "name"
     t.text "description"
     t.string "logo"
-    t.integer "player_count"
+    t.integer "player_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
