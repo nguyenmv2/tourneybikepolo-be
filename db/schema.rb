abacbeb2123f484482e61dcbdc12c0b1829b7bb2
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2018-06-29 00:14:20 -0500

    Add stripe customer id column to User table

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_26_013909) do
+ActiveRecord::Schema.define(version: 2018_06_29_050451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +50,7 @@ ActiveRecord::Schema.define(version: 2018_06_26_013909) do
     t.string "name"
     t.text "description"
     t.string "logo"
-    t.integer "player_count"
+    t.integer "player_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -92,6 +90,7 @@ ActiveRecord::Schema.define(version: 2018_06_26_013909) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "stripe_customer_id", limit: 51
   end
 
   add_foreign_key "enrollments", "teams"
