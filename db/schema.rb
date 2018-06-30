1550f26167f000a3d1029e004b6e137af1863934
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2018-06-29 22:17:46 -0500

    Add money columns to tournament

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_29_050451) do
+ActiveRecord::Schema.define(version: 2018_06_30_030838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 2018_06_29_050451) do
     t.integer "team_cap"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price_cents", default: 0, null: false
+    t.string "price_currency", default: "USD", null: false
   end
 
   create_table "users", force: :cascade do |t|
