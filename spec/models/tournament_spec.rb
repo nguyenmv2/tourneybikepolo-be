b1f8d83e8f62833ea0a053cb967f4e970b3358ac
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2018-06-29 22:28:19 -0500

    Added spec to ensure tournament.fee is tested

--- spec/models/tournament_spec.rb
+++ spec/models/tournament_spec.rb
@@ -7,4 +7,6 @@ describe Tournament, type: :model do
   it { should have_many(:users).through(:tournament_staffs) }
   it { should have_many(:enrollments) }
   it { should have_many(:teams).through(:enrollments) }
+
+  it { is_expected.to monetize(:price_cents).as(:fee) }
 end
