b90d9bce91623491d9ba0751011c9e6db349d453
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2018-08-10 16:59:33 -0500

    Add spec for TimerWorker

--- spec/workers/timer_worker_spec.rb
+++ spec/workers/timer_worker_spec.rb
@@ -2,5 +2,12 @@
 
 require "rails_helper"
 RSpec.describe TimerWorker, type: :worker do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#peform_in" do
+    it "adds the worker to the queue" do
+      m = create(:match, duration: 2.seconds.to_i)
+
+      expect { TimerWorker.perform_in(m.duration, m.timer.id) }
+        .to change(TimerWorker.jobs, :size).by(1)
+    end
+  end
 end
