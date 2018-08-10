9609204eae606b0833166a7d86987679372a874d
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2018-08-10 16:54:06 -0500

    Add documentation for timer worker

--- app/workers/timer_worker.rb
+++ app/workers/timer_worker.rb
@@ -3,6 +3,19 @@
 class TimerWorker
   include Sidekiq::Worker
 
+  # Public: Handles the countdown in the background.
+  #
+  # id - A Timer record id.
+  #
+  # Examples
+  #
+  #   team_one, team_two = Team.last(2)
+  #   match = Match.create(team_one_id: team_one.id, team_two_id: team_two.id, duration: 2.seconds.to_i)
+  #   timer = match.timer
+  #   TimerWorker.perform_async(timer.id)
+  #   # => true
+  #
+  # Returns a boolean or nil depending on if the timer is expired.
   def perform(id)
     timer = Timer.find(id)
 
