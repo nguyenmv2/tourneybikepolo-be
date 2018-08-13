0233483eebf9a2c47ba3b965def7dcbc36c725fc
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2018-08-12 22:27:39 -0500

    Add additional specs for TimerWorker

--- spec/workers/timer_worker_spec.rb
+++ spec/workers/timer_worker_spec.rb
@@ -2,12 +2,32 @@
 
 require "rails_helper"
 RSpec.describe TimerWorker, type: :worker do
-  describe "#peform_in" do
-    it "adds the worker to the queue" do
-      m = create(:match, duration: 2.seconds.to_i)
+  subject { create(:match, duration: 2.seconds.to_i).timer }
 
-      expect { TimerWorker.perform_in(m.duration, m.timer.id) }
-        .to change(TimerWorker.jobs, :size).by(1)
+  it "adds the worker to the queue" do
+    expect { TimerWorker.perform_in(subject.id) }
+      .to change(TimerWorker.jobs, :size).by(1)
+  end
+
+  context "when timer is expired" do
+    it "updates the timer status" do
+      allow_any_instance_of(Timer).to receive(:truly_expired?).and_return(true)
+
+      Sidekiq::Testing.inline! do
+        expect { TimerWorker.perform_async(subject.id) }
+          .to change { subject.reload.status }.to("expired")
+      end
+    end
+  end
+
+  context "when timer is not expired" do
+    it "does nothing" do
+      allow_any_instance_of(Timer).to receive(:truly_expired?).and_return(false)
+
+      Sidekiq::Testing.inline! do
+        expect { TimerWorker.perform_async(subject.id) }
+          .to_not change { subject.reload.status }
+      end
     end
   end
 end
