956fb7c04b090e88b8c1940578daac6282c380ab
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2018-11-28 23:39:11 -0600

    Move rr generator to its own file

--- app/services/round_robin.rb
+++ app/services/round_robin.rb
@@ -4,37 +4,7 @@ module RoundRobin
   def self.schedule(team_ids)
     team_ids.push nil if team_ids.size.odd?
 
-    s = Schedule.new(team_ids)
-    s.build
-  end
-
-  class Schedule
-    def initialize(team_ids)
-      @team_ids = team_ids
-      @team_count = team_ids.size
-      @pivot = team_ids.pop
-      @round_count = (team_count - 1)
-    end
-
-    def build
-      round_count.times.map do
-        team_ids.rotate!
-        construct_round
-      end
-    end
-
-    private
-
-    attr_reader :team_ids, :team_count, :pivot, :round_count
-
-    def construct_round
-      [[team_ids.first, pivot]] + round
-    end
-
-    def round
-      (1...(team_count / 2)).map do |i|
-        [team_ids[i], team_ids[team_count - 1 - i]]
-      end
-    end
+    generator = Generator.new(team_ids)
+    generator.build
   end
 end
